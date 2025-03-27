6048f9b069dc48c6ae80123b15fa13118cfc7dee
Ugo Porcu <ugo.porcu27@gmail.com>
2025-03-27 10:19:34 +0100

    add: shadcn-vue && provider netlify in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+    devtools: { enabled: true },
+    modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: "",
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: "./components/ui",
+    },
 
-  nitro: {
-    preset: 'vercel-edge', // you can use 'vercel' or other providers here
-  },
+    nitro: {
+        preset: "netlify",
+    },
 
-  runtimeConfig: {
-    openaiApiKey: '',
-    assistantId: '',
-  },
+    runtimeConfig: {
+        openaiApiKey: "",
+        assistantId: "",
+    },
 
-  compatibilityDate: '2024-07-05',
-  telemetry: false,
+    compatibilityDate: "2024-07-05",
+    telemetry: false,
 });
